f436614d4b41abce2b2ce9f50d836f89cdcb070b
deVamshi <dev.vamshidhar@gmail.com>
2021-12-07 07:54:32 +0530

    find duplicate using floyds algorithm

--- prbs/5_findduplicate.py
+++ prbs/5_findduplicate.py
@@ -7,6 +7,22 @@ class Solution:
         for i in nums:
             if dupList[i] == 1: return i
 
+# Using floyds algorithm
+# https://www.youtube.com/watch?v=wjYnzkAhcNk
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
 
 # Java Solution
 # class Solution {
