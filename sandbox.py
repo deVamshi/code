dab89f481b530b59bf77354cd667cd36b5ca21e9
deVamshi <dev.vamshidhar@gmail.com>
2022-01-06 19:30:36 +0530

    tried to solve reorganize string problem, not field tested

--- sandbox.py
+++ sandbox.py
@@ -1,22 +1,34 @@
-inp = input()
-isEvenFound = False
-minEven = float('infinity')
-numList = []
+string = "abasijeorjwiegwjijrffffff"
+has = {}
+for i in string:
+    if i in has:
+        has[i] += 1
+    else:
+        has[i] = 1
+freqList = sorted(has.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
 
-for i in inp:
-    if i.isnumeric():
-        if i not in numList:
-            numList.append(i)
-        if int(i) % 2 == 0:
-            isEvenFound = True
-            if int(i) < float(minEven):
-                minEven = i
+for i in range(len(freqList)):
+    freqList[i] = list(freqList[i])
 
-if isEvenFound:
-    numList.remove(minEven)
-    numList.sort(reverse=True)
-    out = "".join(numList)
-    out += minEven
-    print(out)
+loopingCount = 0
+if len(freqList) >= 1:
+    loopingCount = freqList[0][1]
+out = " "
+notPossible = False
+for i in range(loopingCount):
+    for itemSet in freqList:
+        if itemSet[1] == 0:
+            break
+        if out[-1] == itemSet[0]:
+            notPossible = True
+        out += itemSet[0]
+        itemSet[1] -= 1
+    if notPossible:
+        break
+
+if notPossible or loopingCount == 0:
+    print("NOT POSSIBLE" )
 else:
-    print(-1)
+    print(out.strip())
+
+
