d84de73a65c9034b55cf79a21918c58a3cc10d77
deVamshi <dev.vamshidhar@gmail.com>
2022-01-11 09:51:17 +0530

    solved kth largest using sorting

--- ds/arrays/kth_largest.py
+++ ds/arrays/kth_largest.py
@@ -1,5 +1,8 @@
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
 
 
+
+# Sol using a heap
 import heapq
 arr  = [3,2,3,1,2,4,5,5,6]
 k = 2
@@ -17,6 +20,12 @@ for num in arr:
 
 print("out of for loop")
 print(heapq.heappop(pq))
+
+# Sol using sorting
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums = sorted(nums, reverse=True)
+        return nums[k - 1]
 
 
 
