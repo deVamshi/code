8e39448468d3577ec72dc4adf7ae60a7821a417b
Vamshidhar <dev.vamshidhar@gmail.com>
2022-02-01 01:06:16 -0500

    Added solution - LeetHub

--- Count pairs with given sum - GFG/count-pairs-with-given-sum.py
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.py
@@ -4,25 +4,13 @@ class Solution:
     def getPairsCount(self, arr, n, k):
         out = 0
         hashMap = {}
-        for ind, ele in enumerate(arr):
-            if ele in hashMap:
-                hashMap[ele].append(ind)
-            else:
-                hashMap[ele] = [ind]
-                
-        out = 0
-        
-        for ind, ele in enumerate(arr):
-    
-            if k - ele in hashMap:
-                for index in hashMap[k - ele]:
-                    if index > ind:
-                        out += 1
-                        
-        return out
+        for ele in arr:
+            out += hashMap.get(k - ele, 0)
+            hashMap[ele] = hashMap.get(ele, 0) + 1
                         
                     
                 
+        return out
         # for ind in range(len(arr)):
         #     out += arr[ind + 1:].count(k - arr[ind])
         # return out
