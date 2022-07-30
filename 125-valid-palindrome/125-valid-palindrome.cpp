4c73e4cf0f1b7d6cebcacbbe34ea0c226cffca10
Vamshidhar <dev.vamshidhar@gmail.com>
2022-07-30 05:47:42 -0400

    Time: 6 ms (75.53%), Space: 7.4 MB (76.80%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        
+        int left = 0, right = s.size() - 1;
+        
+        while(left <= right){
+            while( left <= right && !isalpha(s[left]) && !isdigit(s[left])) left++;
+            if(left > right) break;
+            while(right >= left && !isalpha(s[right]) && !isdigit(s[right])) right--;
+            if(right < left) break;
+            if(s[left++] != s[right--]) return false;
+        }
+        
+        return true;
+        
+    }
+};
