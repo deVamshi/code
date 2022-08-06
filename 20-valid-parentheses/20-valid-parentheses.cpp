f1bc0b98b46c3901dd8bb40079d0e269be3eaf3a
Vamshidhar <dev.vamshidhar@gmail.com>
2022-08-06 09:30:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.13%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -4,13 +4,14 @@ public:
         unordered_map<char, char> mp = { {')', '('}, {']', '['}, {'}', '{'} };
         stack<char> st;
         
-        for(char ch : s){
+        for(char &ch : s){
             if(mp.find(ch) == mp.end()) st.push(ch);
             else {
                 if(st.empty() || st.top() != mp[ch]) return false;
                 st.pop();
             }
         }
+        
         return st.empty();
     }
 };
